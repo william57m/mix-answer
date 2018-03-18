5ef0ff3714c588c6e80c28147b5137261bc19dd7
William Mura <william.mura@nuance.com>
2018-03-18 16:33:27 -0400

    Adding informations on profile page

--- ui/app/scripts/components/profile/ProfileView.jsx
+++ ui/app/scripts/components/profile/ProfileView.jsx
@@ -1,12 +1,27 @@
 // Lib import
 import React from 'react';
+import { observer } from 'mobx-react';
+
+// App imports
+import SessionStore from '../../stores/session';
 
 
+@observer
 class ProfilePage extends React.Component {
     render() {
+        var user = SessionStore.user || {};
         return (
             <div>
-                Profile View
+                <h3>Profile View</h3>
+                <div className="form-group">
+                    <input type="text" className="form-control" value={user.firstname} disabled />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" value={user.lastname} disabled />
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" value={user.email} disabled />
+                </div>
             </div>
         );
     }

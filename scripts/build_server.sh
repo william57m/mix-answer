74206c9fce61f175aaf6bae53563e9678154a40b
William Mura <william.mura@nuance.com>
2018-03-20 14:04:34 -0400

    Fix build scripts

--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -3,5 +3,5 @@ set -e
 GIT_REVISION=`git describe --long --tags --dirty`
 SERVER_IMAGE=william57m/mix-answer-server
 docker build -t $SERVER_IMAGE:$GIT_REVISION -f ./server/Dockerfile-prod server
-docker tag -f $SERVER_IMAGE:$GIT_REVISION $SERVER_IMAGE:latest
+docker tag $SERVER_IMAGE:$GIT_REVISION $SERVER_IMAGE:latest
 docker push $SERVER_IMAGE:$GIT_REVISION

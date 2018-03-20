76dd9fc2afd5b12c3f6bfa7a60bcbc87ebd2eb9e
William Mura <william.mura@nuance.com>
2018-03-20 11:45:59 -0400

    Add Dockerfile server for the prod

--- scripts/build_server.sh
+++ scripts/build_server.sh
@@ -2,5 +2,5 @@ set -e
 
 GIT_REVISION=`git describe --long --tags --dirty`
 SERVER_IMAGE=william57m/mix-answer-server
-docker build -t $SERVER_IMAGE:$GIT_REVISION -f ./server/Dockerfile server
+docker build -t $SERVER_IMAGE:$GIT_REVISION -f ./server/Dockerfile-prod server
 docker push $SERVER_IMAGE:$GIT_REVISION
